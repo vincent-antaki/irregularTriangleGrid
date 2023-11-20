24b43d3771e40aa8c683a63363ac76f465610a86
Vincent A <git@vincentantaki.com>
2023-11-19 23:53:27 -0500

    triangle grid

--- sketch.js
+++ sketch.js
@@ -4,50 +4,96 @@ let triangleInfo = []
 function setup(){
   randomSeed(0);
 
-  w = min(windowWidth, windowHeight);
-  createCanvas(w, w);
+  w = min(windowWidth-50, windowHeight-50);
 
   // size of the padding between grid and sketch borders
   padding = w/12;
-
+  canvasSize = (w - padding*2)
+  
   // number of rows and columns of the grid
-  gridDivsX = 15;
-  gridDivsY = 15;
+  gridDivsX = 25;
 
-  // actual spacing between grid points
-  gridSpacingX = (w - padding*2)/gridDivsX;
-  gridSpacingY = (w - padding*2)/gridDivsY;
+  // Triangle components - Everything is computed as fraction; assumes that the canvas is a 1x1 square
+  baseTriangleWidth = 2/gridDivsX
+  distanceFromYVertex = baseTriangleWidth/(2*cos(PI/6))
+  YdistanceFromSideVertex = tan(PI/6)*baseTriangleWidth/2
+  XdistanceFromSideVertex = baseTriangleWidth/2
+  baseTriangleHeight = distanceFromYVertex + YdistanceFromSideVertex
 
-  // here we populate the 2d boolean array
+  //Number of rows and canvas height is determined dynamically
+  gridDivsY = Math.floor(1/baseTriangleHeight);
+  canvasHeight = gridDivsY*baseTriangleHeight*canvasSize+2*padding;
+  
+  //console.log(gridDivsY,baseTriangleHeight,canvasSize,2*padding)
+  //console.log(w, canvasHeight)
+  createCanvas(w, canvasHeight);
+  
+  //Create canvas
+  
+  console.log("canvasSize", canvasSize)
+  console.log("nrows", gridDivsY)
+  console.log("baseTriangleHeight",baseTriangleHeight)
+  console.log("distanceFromYVertex", distanceFromYVertex)
+  console.log("YdistanceFromSideVertex", YdistanceFromSideVertex)      
+  console.log("XdistanceFromSideVertex", XdistanceFromSideVertex)
+  baseTriangleHeight
+  
+  //Distance between triangle center 
+  //gridSpacingX = baseTriangleWidth/2
+  
+  //gridSpacingY = (w - padding*2)/gridDivsY;
+
+  
+  
+  // here we populate the 2d boolean array and corresponding center location
   bools = [];
-
   for(let x = 0; x<gridDivsX; x++){
     var column = [];
     for(let y = 0; y<gridDivsY; y++){
       column.push(1);
-    }
+    }    
     bools.push(column);
   }
 
+  // Since points are not regularly space - We use this array to keep track of triangle centers
+  // Padding is not taken into account here
+  centers = [];
+  for(let x = 0; x<gridDivsX; x++){
+    var column = [];
+    for(let y = 0; y<gridDivsY; y++){
+        x_pos = x*baseTriangleWidth/2 + baseTriangleWidth/4
+      if (y%2 ==0){
+        y_pos = y*baseTriangleHeight + (x%2 == 0)*distanceFromYVertex + (x%2 == 1)*YdistanceFromSideVertex
+      } else {
+        y_pos = y*baseTriangleHeight + (x%2 == 1)*distanceFromYVertex + (x%2 == 0)*YdistanceFromSideVertex
+      }
+      
+      //console.log(x, y, x_pos, y_pos)
+      column.push([x_pos, y_pos]);
+
+    }
+    centers.push(column);
+  }
+
+  
   // Draw triangle
+  
   for(let x = 0; x<gridDivsX; x++){
     for(let y = 0; y<gridDivsY; y++){
       is_inverted = ((x+y) %2) == 1
-      x1 = x
-      y1 = y
+      let [cx, cy] = centers[x][y]
+      let [x1, x2, x3] = [cx-baseTriangleWidth/2, cx, cx+baseTriangleWidth/2]
       
       if (is_inverted){
-        x2 = x1-0.5
-        y2 = y1+1
-        x3 = x2+1
-        y3 = y2
+        y1 = cy-YdistanceFromSideVertex 
+        y3 = cy-YdistanceFromSideVertex 
+        y2 = cy+distanceFromYVertex
       } else {
-        x2 = x1+0.5
-        y2 = y1+1       
-        x3 = x1+1
-        y3 = y1
+        y1 = cy+YdistanceFromSideVertex 
+        y3 = cy+YdistanceFromSideVertex 
+        y2 = cy-distanceFromYVertex
       }
-      console.log(x1, y1, x2, y2, x3, y3)
+      //console.log(x1, y1, x2, y2, x3, y3)
       triangleInfo.push([x1, y1, x2, y2, x3, y3])
     }
   }
@@ -57,7 +103,7 @@ function setup(){
   
   background(0);
   stroke(255);
-  strokeWeight(4);
+  strokeWeight(1);
   noFill()
   drawGrid()
   markEmptySpots()
@@ -160,18 +206,12 @@ function drawGrid(){
           r.dimX * gridSpacingX, r.dimY * gridSpacingY)
   }
   
-  console.log("triangleInfo")
-  console.log(triangleInfo[0])
-    console.log(triangleInfo[1])
   for(let n = 0; n<triangleInfo.length; n++){
     let [x1, y1, x2, y2, x3, y3] = triangleInfo[n]
-    console.log(x1, y1, x2, y2, x3, y3)
-    console.log(x1 * gridSpacingX + padding, y1 * gridSpacingY + padding,
-             x2 * gridSpacingX + padding, y2 * gridSpacingY + padding, 
-             x3 * gridSpacingX + padding, y3 * gridSpacingY + padding)
-    triangle(x1 * gridSpacingX + padding, y1 * gridSpacingY + padding,
-             x2 * gridSpacingX + padding, y2 * gridSpacingY + padding, 
-             x3 * gridSpacingX + padding, y3 * gridSpacingY + padding)
+    //console.log(n, x1, y1, x2, y2, x3, y3)
+     triangle(x1 * canvasSize + padding, y1 * canvasSize + padding,
+             x2 * canvasSize + padding, y2 * canvasSize + padding, 
+             x3 * canvasSize + padding, y3 * canvasSize + padding)
     
   }
   
@@ -181,9 +221,11 @@ function markEmptySpots(){
   for(let x = 0; x<gridDivsX; x++){
     for(let y = 0; y<gridDivsY; y++){
       if(bools[x][y]){
-        point(x * gridSpacingX + gridSpacingX/2 + padding,
-              y * gridSpacingY + gridSpacingY/2 + padding)
+        let [x_pos, y_pos] = centers[x][y]
+        point(x_pos * (w - padding*2) + padding,
+              y_pos * (w - padding*2) + padding)
       }
     }
   }
 }
+
