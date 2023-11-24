898106d064e1eb067ffdfde16ea80aa170a26334
Vincent A <git@vincentantaki.com>
2023-11-24 00:03:48 -0500

    Finished making irregular grid

--- sketch.js
+++ sketch.js
@@ -31,12 +31,13 @@ function setup(){
   //Create canvas
   
   console.log("canvasSize", canvasSize)
-  console.log("nrows", gridDivsY)
+  console.log("gridDivsX", gridDivsX )
+  console.log("gridDivsY", gridDivsY)
   console.log("baseTriangleHeight",baseTriangleHeight)
   console.log("distanceFromYVertex", distanceFromYVertex)
   console.log("YdistanceFromSideVertex", YdistanceFromSideVertex)      
   console.log("XdistanceFromSideVertex", XdistanceFromSideVertex)
-  baseTriangleHeight
+  
   
   //Distance between triangle center 
   //gridSpacingX = baseTriangleWidth/2
@@ -67,20 +68,59 @@ function setup(){
       } else {
         y_pos = y*baseTriangleHeight + (x%2 == 1)*distanceFromYVertex + (x%2 == 0)*YdistanceFromSideVertex
       }
-      
-      //console.log(x, y, x_pos, y_pos)
       column.push([x_pos, y_pos]);
-
     }
     centers.push(column);
   }
 
   
-  // Draw triangle
+  // ComputeBaseTriangleInfotriangle
+  // computeBaseTriangleInfo()
+
+  constructIrregularTriangleGrid_1([1, 2, 3])
+
+  background(0);
+  stroke(255);
+  strokeWeight(1);
+  noFill()
+  drawGrid()
+  markEmptySpots()
+}
+
+function makeRect(posX, posY, dimX, dimY){
+  this.posX = posX;
+  this.posY = posY;
+  this.dimX = dimX;
+  this.dimY = dimY;
+}
+
+function randomElement(arr){
+  return arr[(Math.floor(Math.random() * arr.length))]
+}
+
+function is_triangle_inverted(i,j){
+  return ((i+j) %2) == 1
+}
+
+function getTriangleCoordinate(cx, cy, inverted, size){
+  let [x1, x2, x3] = [cx-size/2, cx, cx+size/2]
+
+  a = size/(2*cos(PI/6))
+  b = tan(PI/6)*size/2
+  sign = (1-inverted)*2-1
+
+  y1 = cy+sign*b 
+  y3 = cy+sign*b 
+  y2 = cy-sign*a
+  console.log('coordinates', [x1, y1, x2, y2, x3, y3])
+  return [x1, y1, x2, y2, x3, y3]
   
+}
+
+function computeBaseTriangleInfo(){
   for(let x = 0; x<gridDivsX; x++){
     for(let y = 0; y<gridDivsY; y++){
-      is_inverted = ((x+y) %2) == 1
+      is_inverted = is_triangle_inverted(x,y)
       let [cx, cy] = centers[x][y]
       let [x1, x2, x3] = [cx-baseTriangleWidth/2, cx, cx+baseTriangleWidth/2]
       
@@ -97,118 +137,112 @@ function setup(){
       triangleInfo.push([x1, y1, x2, y2, x3, y3])
     }
   }
-  
-  //constructIrregularGrid([2,3]);
-  //construct([1,2,3])
-  
-  background(0);
-  stroke(255);
-  strokeWeight(1);
-  noFill()
-  drawGrid()
-  markEmptySpots()
 }
 
-function makeRect(posX, posY, dimX, dimY){
-  this.posX = posX;
-  this.posY = posY;
-  this.dimX = dimX;
-  this.dimY = dimY;
+function enumerateBaseTriangle(i,j,size){
+  /*
+  given a grid index i,j corresponding a corner of the triangle and the size of triangle,
+    enumerates all the base triangle that are part of it.
+  */
+  lst = [[i,j]]
+  is_inverted = is_triangle_inverted(i,j)
+
+  for(let s=1; s<size; s++){
+    ydelta = -s*is_inverted+s*(1-is_inverted)
+    lst.push([i,j+ydelta])
+    // Condition was x<=size at some point and that gave quirky shapes
+    for(let xdelta = 1; xdelta<=s; xdelta++){
+      lst.push([i+xdelta,j+ydelta]) 
+      lst.push([i-xdelta,j+ydelta]) // I forgot that line at some point and it gave cool looking overlap
+    }
+  }
+  return lst
 }
 
-function construct(sizesArr){
-  console.log(bools.some(arr => arr.includes(1)))
-
+function getTriangleCenter(i, j, size){
+  // Like the function enumerateBaseTriangle, this functions assumes that i,j describes the
+  //
+  /*
+  h: total height of a triangle, b: distance between center and closest triangle edge, a = h-b
+  here f(x) is the vertical delta between j and the center of a triangle of size s which has triangle i,j as a corner.
   
+  s|x|f(x)
+  1 0 0
+  2 1 2b
+  3 2 2b  a | b + h
+  4 3 2b 2a | 2h
+  5 4 4b 2a | 2b + 2h
+  6 5 4b 3a | b + 3h
+  7 6 4b 4a | 4h    
+    7 6b 4a
+  */
+  
+  // Returns the center of the triangle  
+  [x, y] = centers[i][j] 
+  sign = (1-is_triangle_inverted(i,j))*2-1
+  offset = Math.ceil((size-1)/3)*2*YdistanceFromSideVertex+(size-1-Math.ceil((size-1)/3))*distanceFromYVertex
+  console.log('size', Math.ceil((size-1)/3))
+  console.log('center', sign, offset, x, y)
+  return [x, y+sign*offset]
+}
+
+function getEmptyTriangles(){
+  empties = []
+  for(let x = 0; x<gridDivsX; x++){
+    for(let y = 0; y<gridDivsY; y++){
+      if (bools[x][y]){
+        empties.push([x, y])
+      }
+    }
+  }
+  return empties
+}
+
+function constructIrregularTriangleGrid_1(sizesArr){
+  nattempts = 0
+  ntriangles = 0
   while (bools.some(arr => arr.includes(1))){
-    
-      x = Math.floor(random(gridDivsX))
-      y = Math.floor(random(gridDivsY))
-    
-      xdim = random(sizesArr)
-      ydim = random(sizesArr)
-    
+      // random sample from empty
+      let [x, y] = randomElement(getEmptyTriangles())
+      size = random(sizesArr)
+      console.log(x, y, size)
       fits = true
-
-      console.log(x,y,xdim, ydim)
+      // enumerate all positions
+      positions = enumerateBaseTriangle(x, y, size)
+      console.log(positions)
       // check if within bounds
-      if(x + xdim > gridDivsX || y + ydim > gridDivsY){
+      if(positions.some(a => a[0]<0 | a[1]<0 | a[0]>=gridDivsX | a[1]>=gridDivsY)){
         fits = false
       }
-
-      // check if rectangle overlaps with any other rectangle
       if(fits){
-        fits = !checkIfOverlap(x,y,xdim,ydim)
-      }
-
-      if(fits){
-        addRect(x,y,xdim,ydim)
-      }
-          
-      console.log(x,y,xdim, ydim, fits)
-    
-  }
-  
-}
-
-function addRect(x,y,xdim,ydim){
-      // mark area as occupied
-      for(let xc = x; xc < x + xdim; xc++){
-        for(let yc = y; yc < y + ydim; yc++){
-          bools[xc][yc] = false
+        for(let i = 0; i < positions.length; i++){
+          if (!bools[positions[i][0]][positions[i][1]]){            
+            fits = false
+            break
+          }          
         }
       }
-
-      rectInfo.push(new makeRect(x,y,xdim,ydim))
+        
+      if (fits){
+        for(let i = 0; i < positions.length; i++){
+          console.log('positions', i , positions[i])
+          bools[positions[i][0]][positions[i][1]] = 0
+        }
+        let [cx, cy] = getTriangleCenter(x,y,size)
+        triangleInfo.push(getTriangleCoordinate(cx, cy, is_triangle_inverted(x, y) ,size*baseTriangleWidth))
+        ntriangles += 1
+      }
+      nattempts +=1
+      if (nattempts == 800){
+        break
+      }
+  }  
 }
 
-function checkIfOverlap(x,y,xdim,ydim){
-  for(let xc = x; xc < x + xdim; xc++){
-    for(let yc = y; yc < y + ydim; yc++){
-      if(bools[xc][yc] == 0){
-        return true
-      }
-    }
-  }
-  return false
-}
-
-function constructIrregularGrid(sizesArr){
-  for(let x = 0; x<gridDivsX-max(sizesArr)+1; x++){
-    for(let y = 0; y<gridDivsY-max(sizesArr)+1; y++){
-
-      xdim = random(sizesArr)
-      ydim = random(sizesArr)
-
-      fits = true
-
-      // check if within bounds
-      if(x + xdim > gridDivsX || y + ydim > gridDivsY){
-        fits = false
-      }
-
-      // check if rectangle overlaps with any other rectangle
-      if(fits){
-        fits = !checkIfOverlap(x,y,xdim,ydim)
-      }
-
-      if(fits){
-        addRect(x,y,xdim,ydim)
-      }
-    }
-  }
-}
 
 function drawGrid(){
-  for(let n = 0; n<rectInfo.length; n++){
-    r = rectInfo[n]
-    rect(r.posX * gridSpacingX + padding, r.posY * gridSpacingY + padding,
-          r.dimX * gridSpacingX, r.dimY * gridSpacingY)
-  }
-  
   for(let n = 0; n<triangleInfo.length; n++){
     let [x1, y1, x2, y2, x3, y3] = triangleInfo[n]
-    //console.log(n, x1, y1, x2, y2, x3, y3)
      triangle(x1 * canvasSize + padding, y1 * canvasSize + padding,
              x2 * canvasSize + padding, y2 * canvasSize + padding, 
              x3 * canvasSize + padding, y3 * canvasSize + padding)
